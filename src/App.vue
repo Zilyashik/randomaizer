bbd83d742766a47cbc38e7f30ca4c85e5d0cbf8a
Zilyashik <gzilyam17@mail.ru>
2022-06-22 12:31:50 +0300

    create randomaizer

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <inputForms />
+</template>
+
+<script>
+import inputForms from '@/components/inputForms'
+export default {
+  name: 'App',
+  components: {
+    inputForms,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/inputForms.vue
@@ -0,0 +1,38 @@
+<template>
+  <FormKitSchema :schema="schema" />
+</template>
+
+<script>
+import { FormKitSchema } from '@formkit/vue'
+export default {
+  name: "inputForms",
+  components: FormKitSchema,
+  data() {
+    return {
+      schema: [
+        {
+          $cmp: 'FormKit',
+          props: {
+            type: 'select',
+            id: 'input',
+            label: 'Input',
+            placeholder: 'Pick your input',
+            options: {
+              email: 'Email',
+              number: 'Number',
+              telephone: 'Telephone',
+              text: 'Text',
+              textarea: 'Textarea'
+            },
+            validation: 'required'
+          }
+        },
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
