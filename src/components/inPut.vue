be86e40a49ff1f258f8b4fc521ec45243e275648
Zilyashik <gzilyam17@mail.ru>
2022-06-27 16:04:37 +0300

    delete

---
+++ src/components/inPut.vue
@@ -0,0 +1,28 @@
+<template>
+ <div class="inputs_wrapper">
+   <div class="inputs_container">
+     <div class="input_container">
+       <input type="select"
+       placeholder="">
+     </div>
+   </div>
+ </div>
+</template>
+
+<script>
+export default {
+  name: "inPut"
+}
+</script>
+
+<style scoped>
+  .inputs_wrapper{
+
+  }
+  .inputs_container{
+
+  }
+  .input_container{
+
+  }
+</style>
