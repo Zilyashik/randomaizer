3b268f6149c7002e258d8c4980bc0f8ef5afc920
Zilyashik <gzilyam17@mail.ru>
2022-07-04 02:04:21 +0300

    Delete trash

--- src/App.vue
+++ src/App.vue
@@ -7,55 +7,9 @@
 </template>
 
 <script>
-import {components} from "@/inputs/components";
 
 export default {
   name: 'App',
-  data() {
-    return {
-      forms: {
-        count: 0,
-        components: []
-      },
-      schema: {
-       ...components
-      }
-    }
-  },
-  methods: {
-    addInput(index, type) {
-      let schema = this.schema;
-      this.forms.components = this.forms.components.map(
-          function (t) {
-            if (t.index === index) {
-              t.type = schema[type];
-            }
-            return t;
-          });
-    },
-    create() {
-      for (let i = 1; i <= this.forms.count; i++) {
-        this.forms.components.push({
-          index: i,
-          type: this.schema['email']
-        });
-      }
-    },
-    add() {
-      this.forms.count++;
-      this.forms.components.push({
-        index: this.forms.count,
-        type: this.schema['email']
-      });
-    },
-    del() {
-      this.forms.count--;
-      this.forms.components.pop(this.schema['email']);
-    },
-    removeInput(index) {
-      this.forms.components = this.forms.components.filter(t => t.index !== index);
-    },
-  }
 }
 </script>
 
